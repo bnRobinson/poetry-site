6b5bc747487a9f50f276602b17ce9eaa2055bad2
Brittany Robinson <bncopple@gmail.com>
2024-02-03 08:34:56 -0600

    Initial commit, spring boot initialized

---
+++ robinson-poetry/src/main/java/com/practice/robinsonpoetry/RobinsonPoetryApplication.java
@@ -0,0 +1,13 @@
+package com.practice.robinsonpoetry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobinsonPoetryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobinsonPoetryApplication.class, args);
+	}
+
+}
---
+++ robinson-poetry/src/test/java/com/practice/robinsonpoetry/RobinsonPoetryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.robinsonpoetry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobinsonPoetryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
