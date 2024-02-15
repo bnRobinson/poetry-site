80d367735055e8ec4c3f8133f2fed68a978ee718
Brittany Robinson <bncopple@gmail.com>
2024-02-15 15:41:37 -0600

    controller updated to render creative writing

--- robinson-poetry/src/main/java/com/practice/robinsonpoetry/controllers/AdministratorHomeController.java
+++ robinson-poetry/src/main/java/com/practice/robinsonpoetry/controllers/AdministratorHomeController.java
@@ -2,15 +2,39 @@ package com.practice.robinsonpoetry.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class AdministratorHomeController {
 
-    @GetMapping ("/add")
+
+    private static int nextId = 1;
+
+    public final static Map<Integer, String> poems = new HashMap<>() {{
+        put(1, "Autumn");
+        put(2, "Baby Thoughts");
+        put(3, "Dear Mama");
+        put(4, "Frostbite");
+    }};
+
+    @GetMapping("/addPoem")
     @ResponseBody
-    public String renderAddWritingPage ()
-    {
-        return "<h2>Add New Writing<h2>";
+    public String renderAddPoemPage(@RequestParam String poem) {
+
+        poems.put(nextId, poem);
+        nextId ++;
+        return "<html>" +
+                "<body>" +
+                "<h2>" +
+                "Poem Successfully Added!"+
+                "<h2>" +
+                "<p> Your poem" + poem + "has been added to the poetry collection <p>" +
+                "</body>" +
+                "</html>";
+
     }
 }
